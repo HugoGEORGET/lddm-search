4321c18c8549457d09b6bbe6959efaca61713318
Hugo Georget <hugo.georget@sword-group.com>
2020-02-04 14:22:51 +0100

    Updates

---
+++ precache-manifest.d51a48d3b83cbbea439dac37fd277884.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52adf7c259f8d696c5e1184d8cc772e9",
+    "url": "/lddm-search/index.html"
+  },
+  {
+    "revision": "b363d75a5b65f271d87c",
+    "url": "/lddm-search/static/css/main.7dd3d643.chunk.css"
+  },
+  {
+    "revision": "6396eeb14c69e651f288",
+    "url": "/lddm-search/static/js/2.f92ce506.chunk.js"
+  },
+  {
+    "revision": "b681d4f3bcbfbec2048976ff8b2b9df3",
+    "url": "/lddm-search/static/js/2.f92ce506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b363d75a5b65f271d87c",
+    "url": "/lddm-search/static/js/main.cd5e91f3.chunk.js"
+  },
+  {
+    "revision": "3bd9bb566d491f7cda80",
+    "url": "/lddm-search/static/js/runtime-main.b7a18c32.js"
+  }
+]);
